94fb3888dd97a6129c4f7199d7aecb393e8c29d6
vism-7 <your-github-bhishamchauhan43@gmail.com>
2026-05-20 14:37:49 +0530

    clean: remove unused database files and api folder

--- db/config.ts
+++ db/config.ts
@@ -1,14 +1,14 @@
-// https://astro.build/db/config
-import { defineDb, defineTable, column } from 'astro:db';
+// // https://astro.build/db/config
+// import { defineDb, defineTable, column } from 'astro:db';
 
-const Feedback = defineTable({
-  columns: {
-    slug: column.text({ primaryKey: true }),
-    helpful: column.number({ default: 0 }),
-    notHelpful: column.number({ default: 0 }),
-  },
-});
+// const Feedback = defineTable({
+//   columns: {
+//     slug: column.text({ primaryKey: true }),
+//     helpful: column.number({ default: 0 }),
+//     notHelpful: column.number({ default: 0 }),
+//   },
+// });
 
-export default defineDb({
-  tables: { Feedback },
-});
+// export default defineDb({
+//   tables: { Feedback },
+// });
